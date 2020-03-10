2501bd49b99c0f219674398ee125c8a6726309c8
Surya Jamalpuri <SJamalpuri@ad.prolifics.com>
2020-03-10 21:15:39 +0530

    created server comp, wip with other funcs

--- recipeShoppingApp/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipeShoppingApp/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
-
+recipes:Recipe[] = [
+  new Recipe('my first recipe', 'my first description', 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg'),
+  new Recipe('my 2nd recipe', 'my first description', 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg'),
+  new Recipe('my 3rd recipe', 'my first description', 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg')
+];
   constructor() { }
 
   ngOnInit() {
--- recipeShoppingApp/src/app/shopping-list/shopping-list.component.ts
+++ recipeShoppingApp/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingrediant } from '../shared/ingrediand.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.scss']
 })
 export class ShoppingListComponent implements OnInit {
-
+ingrediants: Ingrediant[] = [
+  new Ingrediant('qwdqdwqd', 84884),
+  new Ingrediant('sdf', 232),
+  new Ingrediant('rtc', 65)
+]
   constructor() { }
 
   ngOnInit() {
